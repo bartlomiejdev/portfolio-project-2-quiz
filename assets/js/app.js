792d9c5f7611930bc1ba1a9707e68791af2079d2
bartlomiejdev <rzeznia777@gmail.com>
2022-10-27 19:47:51 +0000

    Set correctAnswer to empty string and set total number of questions for 15

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
 const question = document.getElementById("question");
 const options = document.querySelector(".quiz-options");
 
+let correctAnswer = "",
+  correctScore = (askedCount = 0),
+  totalQuestion = 15;
+
 // fetch questions from API
 async function loadQuestion() {
     const url = "https://the-trivia-api.com/api/questions?categories=sport_and_leisure&limit=15&difficulty=medium";
@@ -8,4 +12,8 @@ async function loadQuestion() {
     const data = await results.json();
     result.innerHTML = "";
     showQuestion(data[0]);
-  }
+}
+
+function showQuestion(data) {
+
+}
